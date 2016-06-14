e3bc068aa223853dd6dac26f7a39650332dd5de4
pantel77 <7pantel7@gmail.com>
2016-06-14 15:44:54 +0300

    add headers

---
+++ SPaniukov/search/searchs.cpp
@@ -0,0 +1,55 @@
+#include "searchs.hpp"
+#include <cassert>
+
+int search_1(int arr[], int size, int key)
+{
+   assert(arr != 0);
+   assert(size >= 0);
+
+   for (int i = 0; i < size; i++)
+   {
+      if (arr[i] == key) return i;
+   }
+   return -1;
+}
+
+int search_2(int arr[], size_t size, int key)
+{
+   assert(arr != 0);
+
+   size_t i = 0;
+   while (i < size)
+   {
+      if (arr[i] == key) return i;
+      i++;
+   }
+   return -1;
+}
+
+int search_3(int arr[], size_t size, int key)
+{
+   assert(arr != 0);
+
+   arr[size] = key;
+   size_t i = 0;
+   while (arr[i] != key) i++;
+   if (size != i) return i;
+   else return -1;
+}
+
+int search_4(int arr[], size_t size, int key)
+{
+   assert(arr != 0);
+
+   size_t last = size - 1;
+   int end = key;
+   int save = arr[last];
+   if (arr[last] == end) return last;
+   else arr[last] = end;
+
+   size_t i = 0;
+   while (arr[i] != key) i++;
+   arr[last] = save;
+   if (last != i) return i;
+   else return -1;
+}
---
+++ SPaniukov/search/searchs.hpp
@@ -0,0 +1,10 @@
+#ifndef SEARCHS_HPP
+#define SEARCHS_HPP
+
+int search_1(int arr[], int size, int key);
+int search_2(int arr[], size_t size, int key);
+int search_3(int arr[], size_t size, int key);
+int search_4(int arr[], size_t size, int key);
+
+#endif
+
