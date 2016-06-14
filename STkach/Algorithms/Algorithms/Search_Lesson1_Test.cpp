212e15b299690ed9cb0245efb9145e1081d34dbd
Stanislav Tkach <scatcher07@gmail.com>
2016-06-14 12:03:32 +0300

    tests for search

---
+++ STkach/Algorithms/Algorithms/Search_Lesson1_Test.cpp
@@ -0,0 +1,13 @@
+#include "Search_Lesson1.cpp"
+
+bool zeroSizeArray();
+bool oneElementArrayExist();
+bool oneElementArrayNotExist();
+bool twoElementsArrayExist();
+bool twoElementsArrayNotExist();
+bool searchElementFirst(); // 5 elements array
+bool searchElementLast();  // 5 elements array
+bool searchElementMid();   // 5 elemnets array
+bool searchElementsNotExist() // 5 elements array
+
+bool zeroSizeArray()
