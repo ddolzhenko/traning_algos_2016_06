b948d265f7636b4a33baca5909226a2e1696b4da
amolodchenkova <molodchenkova@gmail.com>
2016-06-21 12:55:44 +0300

    bounds and remove_if

---
+++ dmaliavin/remove_if.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <string>
+#include <typeinfo>
+#include <algorithm>
+
+int* remove_even(int* first, int* last)
+{
+    assert(first <= last);
+    int* res = first;
+    while(first != last)
+    {
+        if(*first % 2 != 0)
+        {
+            *res = *first;
+            res++;
+        }
+        first++;
+    }
+    return res;
+}
+
+int main()
+{
+    int size = 4;
+    int A[] = {6, 1,2,3,4};
+    int* res = remove_even(A, A+size);
+    for(int* p = A; p != res; p++)
+    {
+        std::cout<< *p << "\n";
+    }
+    return 0;
+}
