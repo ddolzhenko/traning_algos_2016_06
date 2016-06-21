9a9a40e4292e8fcf87c821b6efcb0a3d66ebd79c
kurzgame <kurzgame@gmail.com>
2016-06-21 12:09:13 +0300

    update

--- osmorchkov/min.cpp
+++ osmorchkov/min.cpp
@@ -22,9 +22,9 @@ int *bin_search1(int *b, int *e, int key)
 
         int *m = b + (e - b) / 2;
         if (*m < key)
-            e = m; // [b, m)
-        else if (*m > key)
             b = m + 1; // [m ,e)
+        else if (*m > key)
+            e = m; // [b, m)
         else
             return m; // [m]
     }
@@ -47,6 +47,22 @@ int *lower_bound(int *b, int *e, int key)
     return e;
 }
 
+// first element of the sequence
+// always log N
+int *upper_bound(int *b, int *e, int key)
+{
+    while (b < e)
+    {
+        int *m = b + (e - b) / 2;
+
+        if (key < *m) // only one comprassion
+            e = m;
+        else
+            b = m + 1;
+    }
+    return e;
+}
+
 // always 1 + log N
 int *bin_search2(int *b, int *e, int key)
 {
@@ -55,6 +71,22 @@ int *bin_search2(int *b, int *e, int key)
     if (lb != e && *lb == key) // DZ: write wihout comprasion operator!!!
         return lb;
     return e;
+}
+
+// [0, 1, 2, 3, x, x, x, x, x, 10, 11]
+int count(int *b, int *e, int key)
+{
+    // return upper_bound - lower_bound;
+    int c = 0;
+
+    int *lb = lower_bound(b, e, key);
+
+    if (lb != e)
+    {
+        while (*lb++ == key) ++c;
+    }
+
+    return c;
 }
 
 int main()
