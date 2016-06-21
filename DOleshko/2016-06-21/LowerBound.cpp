ed3a0c65787e6544b0d8a2b5229c2f4189abb70e
ww <ww>
2016-06-21 10:49:46 +0300

    Open intervals usage: Linear / Binary Search / Lower Bound

---
+++ DOleshko/2016-06-21/LowerBound.cpp
@@ -0,0 +1,36 @@
+
+
+template <class TIter, class T>
+TIter lower_bound(TIter b, TIter e, const T& key)
+{
+    while (b < e)
+    {
+        TIter m = b + (e - b)/2;
+
+        // [b, m) U [m] U [m+1, e)
+
+        if (*m < key) 
+        {
+            b = m + 1;     // all keys are to the right
+        }
+        else 
+        {
+            e = m;         // some keys are to the left
+        }
+    }
+
+    return b;
+}
+
+template <class TIter, class T>
+TIter binary_search(TIter b, TIter e, const T& key)
+{
+    auto lb = lower_bound(b, e);
+
+    if (lb != e && *Lb == key)  // HW: USE ONLY < !!!!!!
+    {
+        return lb;
+    }
+
+    return e;
+}
