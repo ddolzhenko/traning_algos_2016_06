f528c2f9e0dc80ef0801105175ce756d0218f2d5
pantel77 <7pantel7@gmail.com>
2016-06-23 13:14:58 +0300

    naive, selection, bubble and insertion sort

--- SPaniukov/search/boundUpLowRemoveIf.hpp
+++ SPaniukov/search/boundUpLowRemoveIf.hpp
@@ -8,44 +8,44 @@ TIter lowerBound(TIter begin, TIter end, const T& key)
 {
 	assert(std::is_sorted(begin, end));
 
-	while (begin < end)
+	while (begin < end)//<1
 	{
-		TIter middle = begin + (begin - end) / 2;
+		TIter middle = begin + (begin - end) / 2;//4
 		// [begin, middle) and [middle] and (middle, end)
-		if (*middle < key) begin = middle + 1; //[middle] and (middle, end)
+		if (*middle < key) begin = middle + 1; //[middle] and (middle, end)//log4n
 		else end = middle;// [begin, middle)
 	}
-	return end;
+	return end;//1
 }
 
 template<class TIter, class T>
-TIter upperBound(TIter begin, TIter end, const T& key)
+TIter upperBound(TIter begin, TIter end, const T& key)          //O(1)
 {
 	assert(std::is_sorted(begin, end));
 
-	while (begin < end)
+	while (begin < end)                                         //O(log n)
 	{
-		TIter middle = begin + (begin - end) / 2;
-		// [begin, middle) and [middle] and (middle, end)
-		if (key < *middle) end = middle; // [begin, middle)
-		else begin = middle+1;//[middle] and (middle, end)
+		TIter middle = begin + (begin - end) / 2;               //O(1)
+		// [begin, middle) and [middle] and (middle, end)       //O(1)
+		if (key < *middle) end = middle; // [begin, middle)     //O(1)
+		else begin = middle+1;//[middle] and (middle, end)      //O(1)
 	}
-	return begin;
+	return begin;                                               //O(1)
 }
 
 template <class TIter, class T>
-TIter binary_search(TIter begin, TIter end, const T& key) {
-   auto lb = lowerBound(begin, end, key);
-   TIter iter = end;
-   if (lb != end && *lb == key) iter = lb;;
-   return iter;
-}
+TIter binary_search(TIter begin, TIter end, const T& key) {//= 3
+   auto lb = lowerBound(begin, end, key);//=4+log4n+6
+   TIter iter = end;//1
+   if (lb != end && *lb == key) iter = lb;//4
+   return iter;//1
+}//complexity log4n+18
 
 
 template <class TIter>
-size_t count_7(TIter b, Titer e)
+size_t count_7(TIter b, Titer e)//O(1)
 {
-   return upperBound(b, e, 7) - lowerBound(b, e, 7);
+   return upperBound(b, e, 7) - lowerBound(b, e, 7);//O(1)+O(log n) + O(log n)== O(log n)
 }
 
 template<class TIter>
@@ -83,7 +83,7 @@ TIter remove_if(TIter begin, TIter end, TFunc predicat)
    {
       //[begin, even) and [even] and (even, end)
       if (!predicat(begin))//[begin, even) and [even]
-      {
+      { 
          *temp = *begin;
          temp++;
       }
@@ -91,5 +91,5 @@ TIter remove_if(TIter begin, TIter end, TFunc predicat)
    }
    return temp;
 }
-
+//test
 #endif
---
+++ SPaniukov/search/sort.hpp
@@ -0,0 +1,87 @@
+#ifndef SORT_HPP
+#define SORT_HPP
+#include "boundUpLowRemoveIf.hpp"
+#include <cassert>
+
+template<class T>
+bool isMore(const &T vol1, const &T vol2)
+{
+	return *vol1 < *vol2;
+}
+
+template<class TIter>
+void iter_swap(TIter vol1, TIter vol2)
+{
+	auto temp = *vol1;
+	*vol1 = *vol2;
+	*vol2 = *vol1;
+}
+
+template<class TIter>
+void naive_sort(TIter begin, TIter end)
+{
+	for (TIter iter = begin; iter < end; iter++)
+	{
+		assert(std::is_sorted(begin, iter));// [sorted)[iter][usorted)
+		for (TIter iter_ = iter+1; iter_ < end; begin++)
+		{
+			assert(iter = std::min_element(iter, iter_));//[iter][iter_] usorted)
+			if (!isMore(iter, iter_)) iter_swap(iter, iter_);
+		}
+	}
+}
+
+template<class TIter>
+void selection_sort(TIter begin, TIter end)
+{
+	for (TIter iter = begin; iter < end; begin++)//O(n)
+	{
+		iter_swap(iter, searchPointerMinElement(iter, end));//O(n*n)
+	}
+}
+
+template<class TIter>
+void bubble_sort(TIter begin, TIter end)
+{
+	for (TIter iter = end; iter != begin; iter--)
+	{
+		//[usorted)[sorted)
+		//[begin, iter)[iter, end)
+		assert(is_sorted(iter, end));
+		for (TIter iter_ = begin+1; iter_ < iter; iter_++)
+		{
+			//[begin, iter_)[iter_][next][next+1, end)
+			assert(iter_ == max_element(begin, iter_));
+			auto previus = iter_ - 1;
+			if (*iter_ < *previus) iter_swap(previus, iter_);
+		}
+		assert(is_sorted(iter, end));
+	}
+}
+
+template<class TIter>
+void insertion_sort(TIter begin, TIter end)
+{
+	//[sorted][usorted)
+	//[begin, iter)[iter, end)
+	for (TIter iter = begin; iter < end; iter++)
+	{
+		assert(is_sorted(begin, iter));
+		for (TIter iter_ = iter; iter_ > begin; iter_--)
+		{
+			//[begin, iter_)[iter_][iter_+1, iter+1)
+			assert(is_sorted(begin, iter_) && is_sorted(iter_, iter));
+			
+			auto prev = iter_ - 1;
+			if (*iter_ < prev) 
+				iter_swap(prev, iter_);
+			else 
+				break;
+			
+			assert(is_sorted(begin, iter_) && is_sorted(iter_, iter));
+		}
+		assert(is_sorted(begin, iter));
+	}
+}
+
+#nedif 
