a73a9086df2452218743ccaa63fd141cec7adab5
pantel77 <7pantel7@gmail.com>
2016-08-04 12:52:36 +0300

    +bfs

--- SPaniukov/BTrees.py
+++ SPaniukov/BTrees.py
@@ -36,6 +36,34 @@ def remove_if(seq, pred):
 		if not pred(x):
 			yield x
 
+def height(tree):
+	pass
+
+class Queue:
+	def __init__(self):
+		self._data = []
+
+	def push(self, x):
+		self._data.append(x)
+
+	def pop(self):
+		return self._data.pop(0)
+
+	def empty(self):
+		return len(self._data) == 0
+
+def bfs(tree):
+	queue = Queue()
+	queue.push(tree)
+	
+	while not queue.empty():
+		node = queue.pop()
+
+		if node:
+			yield node
+			queue.push(node.left)
+			queue.push(node.right)
+
 def create_tree():
 	t = Tree(8,
 			Tree(3,
@@ -58,21 +86,30 @@ def main():
     #print("****************************")
     #dfs_post_order(t, print)
     #print("****************************")
-    
+    print("****************************")
     for node in dfs_nodes(t):
     	print(node)
+    
     print("****************************")
     print("---".join(map(str, dfs_nodes(t))))
+    
     print("****************************")
     it = dfs_nodes(t)
     for x in range(4):
     	print (next(it))
+    
     print("****************************")
     for x in remove_if(dfs_nodes(t), lambda x: x.data%3 != 0):
     	print (x)
+    
     print("****************************")
     print(", ".join(map(str, remove_if(dfs_nodes(t), lambda x: x.data%3 != 0))))
-    print("****************************")
 
+    print("****************************")
+    for x in bfs(t):
+    	print(x)
+    print(list(map(str, bfs(t))))
+    
 if __name__ == '__main__':
 	main()
+
